123d3a97a8dc68b1cde2e220cc4735d2210a25c9
Oleg Tyurin <oleg.tyurin.external@enmacc.com>
2024-04-20 21:42:15 +0200

    add menu-card component

---
+++ src/app/components/menu-card/menu-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-menu-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './menu-card.component.html',
+  styleUrls: ['./menu-card.component.scss']
+})
+export class MenuCardComponent {
+
+}
