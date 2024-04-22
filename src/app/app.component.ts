ecedac896ec58413d3cd0f4ee9c2cb457ade64fb
Oleg Tyurin <oleg.tyurin.external@enmacc.com>
2024-04-22 13:07:22 +0200

    add font-family to titles and descriptions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest, distinctUntilChanged, map } from 'rxjs';
 import { BasketService } from './basket.service';
 import { UiDataStateService } from './ui-data-state.service';
 
@@ -13,6 +13,7 @@ export class AppComponent implements OnInit {
   private stateService = inject(UiDataStateService);
   isBasketOpen$ = this.basketService.isOpen$;
   totalAmountInBasket$: Observable<number> = this.stateService.totalAmountInBasket$;
+  showBasketButton$ = combineLatest([this.isBasketOpen$, this.totalAmountInBasket$]).pipe(map(([isOpen, count]) => !isOpen && count > 0), distinctUntilChanged());
 
 
   ngOnInit(): void {
